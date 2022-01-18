d191fc81596e95c45ad0ebd271cf77a75ba8a905
DenardYap <mameehy@hotmail.com>
2022-01-17 23:55:37 -0500

    1) Fix overflow bug 2) Ameliorated footer 3) Added links to contacts and social media 4) Fix footer doesn't flow in the middle when there's only 1 post

--- blog-app/src/Footer.js
+++ blog-app/src/Footer.js
@@ -10,17 +10,47 @@ import {
 import { AiOutlineMail } from "react-icons/ai";
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer className="footer">
       <div className="footer-normal">
         <IconContext.Provider value={{ className: "icons" }}>
-          <FaFacebook />
-          <FaGithub />
-          <FaInstagram />
-          <FaLinkedin />
-          <FaTwitterSquare />
+          <a
+            href="https://www.facebook.com/kahhuan.yap.5"
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://github.com/DenardYap"
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.instagram.com/bernutyap123/"
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bernard-yap/"
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://twitter.com/"
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <FaTwitterSquare />
+          </a>
         </IconContext.Provider>
         <h4 className="footer-font">
-          Design and programmed by Bernard Kah Huan Yap
+          Designed and programmed by Bernard Kah Huan Yap
         </h4>
         <h5 className="footer-font-2">
           Copyright @ 2022 - 2023 | All rights reserved |{" "}
@@ -30,12 +60,16 @@ const Footer = () => {
       <div className="footer-contact">
         <h3>Contact information</h3>
         <span className="email-wrapper">
-          <h4>bernerd@umich.edu</h4>
-          <AiOutlineMail size={"10%"} />
+          <a href="mailto:bernerd@umich.com">
+            <h4>bernerd@umich.edu</h4>
+          </a>
+          <a href="mailto:bernerd@umich.com">
+            <AiOutlineMail size={"30%"} cursor={"pointer"} />
+          </a>
         </span>
         <h5>+1734-934-2963 | ANN ARBOR, MI 48105</h5>
       </div>
-    </div>
+    </footer>
   );
 };
 
