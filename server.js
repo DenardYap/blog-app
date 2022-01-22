dd029cb75375f8fc3677b90c672b60b6b2c3cea3
DenardYap <mameehy@hotmail.com>
2022-01-22 11:35:14 -0500

    still playing around

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use("/api/posts", require("./api/posts"));
 //   res.sendFile(path.join(__dirname, "notfound.html"));
 // });
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  console.log("lolol");
+  res.sendFile(path.join(__dirname + "/blog-app/build/index.html"));
 });
 const PORT = process.env.PORT || 3001;
 
